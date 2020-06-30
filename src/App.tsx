cf7795fa621eb9120cb6e1fed5f7b7bc25c05cbb
Takahashi Takuya <takahashitakuya031126@gmail.com>
2020-06-30 11:18:37 +0900

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TaskList from './components/TaskList'
+import TaskInput from './components/TaskInput'
+import { Task } from './Types'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState: Task[] = [
+  {
+      id: 2,
+      title: '次にやること',
+      done: false
+  },{
+      id: 1,
+      title: 'はじめにやること',
+      done: true
+  }
+]
+
+const App: React.FC = () => {
+const [tasks, setTasks] = useState(initialState)
+
+return (
+  <div>
+      <TaskInput setTasks={setTasks} tasks={tasks} />
+      <TaskList setTasks={setTasks} tasks={tasks} />
+  </div>
+)
 }
 
 export default App;
